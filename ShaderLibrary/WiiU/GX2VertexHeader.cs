1396568067659f779f80d8745e50a87f04963091
KillzXGaming <millernathan35@gmail.com>
2024-05-02 18:08:52 -0400

    Fix missing wii u gx2 headers

---
+++ ShaderLibrary/WiiU/GX2VertexHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BfshaLibrary.WiiU
+{
+    public class GX2VertexHeader
+    {
+        public byte[] Data { get; set; }
+        public uint[] Regs { get; set; }
+        public uint Mode { get; set; }
+    }
+}
